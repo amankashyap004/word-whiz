0da0d98a7ea55523ede04d9a72968a65178f2937
amankashyap004 <amankashyap004@gmail.com>
2023-06-22 19:19:12 +0530

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 
 const Header = () => {
-   return <div>Header</div>;
+   return (
+      <header className="bg-gray-900 text-white py-4">
+         <div className="container mx-auto px-4">
+            <nav className="flex items-center justify-between">
+               <h1 className="text-2xl font-bold cursor-pointer">
+                  <span className="text-red-500">W</span>ord
+                  <span className="text-red-500">W</span>hiz
+               </h1>
+               <ul className="flex items-center justify-center text-base">
+                  <li className="px-2 cursor-pointer font-semibold uppercase hover:text-red-500">
+                     Home
+                  </li>
+                  <li className="px-2 cursor-pointer font-semibold uppercase hover:text-red-500">
+                     About
+                  </li>
+                  <li className="px-2 cursor-pointer font-semibold uppercase hover:text-red-500">
+                     Setting
+                  </li>
+               </ul>
+            </nav>
+         </div>
+      </header>
+   );
 };
 
 export default Header;
