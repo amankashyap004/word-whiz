38eebf63cb7995b9bf74972c72404419289931a5
amankashyap004 <amankashyap004@gmail.com>
2023-06-23 18:15:08 +0530

    home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 
 const Home = () => {
-   return <div>Home</div>;
+   return (
+      <main className="flex items-center justify-center flex-col h-full bg-slate-800 py-4">
+         <div className="flex items-center justify-center flex-col p-2">
+            <h1 className="text-2xl font-bold text-white p-2">Welcome to Word Whiz</h1>
+            <h3 className="text-lg font-bold text-slate-400">
+               The ultimate typing game for mastering English spelling!
+            </h3>
+         </div>
+         <div className="flex items-center justify-center flex-col p-2 w-1/2">
+            <p className="text-lg font-normal text-zinc-300 text-center">
+               Test and improve your typing skills while sharpening your spelling abilities.
+               Challenge yourself with a variety of engaging word exercises and see how fast and
+               accurately you can type!
+            </p>
+         </div>
+         <div className="bg-blue-700 px-4 py-2 my-4 cursor-pointer rounded">
+            <h2 className="text-lg text-white">Start playing now and become a Word Whiz!</h2>
+         </div>
+      </main>
+   );
 };
 
 export default Home;
